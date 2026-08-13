312f1d5acbc8cfea1f3935aa53e1882aa5bb18b6
dcharlon <david@charlon.de>
2026-08-13 15:07:14 +0200

    CS50P PS0: Aufgabe 'indoor' gelöst

---
+++ lecture0/indoor.py
@@ -0,0 +1,4 @@
+# What is the method call to force all letters of the output to be lower case? Bruh its called "lower"
+provocation = input("WHATEVER YOU TYPE, IT WILL BE RETURNED WITHOUT CAPS. GO AHEAD!!! ").lower()
+# you could use print ("as you can see:'",provocation, "'is not written in caps",sep='') or what I used for simplicity
+print (provocation)
